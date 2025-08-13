a9d4495ccf9c4ffa2639a37317618b2da7732408
grice728 <grice728@yahoo.com>
2025-08-13 14:00:28 -0400

    Create singlecare.py

---
+++ scrapers/scrapers/singlecare.py
@@ -0,0 +1,7 @@
+# scrapers/singlecare.py
+from typing import List
+from models import PriceResult
+
+def scrape(drug: str, dosage: str, quantity: str, zip_code: str) -> List[PriceResult]:
+    # TODO: implement real SingleCare scrape
+    return []
